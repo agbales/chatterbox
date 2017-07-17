9351f5e6ede5389bcfd5bab6ae153b7e5f06d0cb
Andrew Bales <agbales@gmail.com>
2017-07-16 22:19:29 -0700

    adding socket.io

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var path = require('path');
 var express = require('express');
 var port = process.env.port || 8080
 var app = express();
+var server = app.listen(port);
+var io = require('socket.io')(server);
 
 var staticPath = path.join(__dirname, '/dist');
 app.use(express.static(staticPath));
 
-app.listen(port, function() {
-  console.log('listening @ ' + port);
+io.on('connection', function(socket){
+  console.log('a user connected @ : ', socket);
+  socket.on('message', msg => console.log(msg));
 });
--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import style from './css/style.css'
+import { SocketProvider, socketConnect } from 'socket.io-react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
+  componentDidMount() {
+  }
+  sendMessage() {
+    socket.emit('message', 'Hello world!');
+  }
   render() {
     return (
       <div className="user-bubble">
         <h1>Howdy.</h1>
+        <button onClick={this.sendMessage}>
+          Send!
+        </button>
       </div>
     );
   }
