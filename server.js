39346f7360f8cdf04557498fb716bd8856346e1c
Andrew Bales <agbales@gmail.com>
2017-07-16 21:39:59 -0700

    port variable

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var path = require('path');
 var express = require('express');
-
+var port = process.env.port || 8080
 var app = express();
 
 var staticPath = path.join(__dirname, '/dist');
 app.use(express.static(staticPath));
 
-app.listen(3000, function() {
-  console.log('listening @ 3000');
+app.listen(port, function() {
+  console.log('listening @ ' + port);
 });
