6b018a4e29d1e7edfedcf20f3af5a7172ad0e2c8
Andrew Bales <agbales@gmail.com>
2017-09-14 12:47:46 -0700

    create input field, begin binding

--- src/app/index.js
+++ src/app/index.js
@@ -5,15 +5,26 @@ import { SocketProvider, socketConnect } from 'socket.io-react';
 import io from 'socket.io-client';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      words: 'chatterbox'
+    }
+  }
   componentDidMount() {
   }
   sendMessage() {
     socket.emit('message', 'Hello world!');
   }
+  updateWords(event) {
+    console.log('hi..' + this.state.words)
+    this.setState({ words: event.target.value })
+  }
   render() {
     return (
       <div className="user-bubble">
-        <h1>Howdy.</h1>
+        <input type="text" value={this.state.words} onChange={this.updateWords} />
         <button onClick={this.sendMessage}>
           Send!
         </button>
